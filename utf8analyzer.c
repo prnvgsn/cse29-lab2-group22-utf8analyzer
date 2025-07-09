6a78e1fea08f678585947c2994ce8076e38a4320
Park <akp007@ieng6-203.ucsd.edu>
2025-07-09 12:12:39 -0700

    reset test

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -11,12 +11,4 @@ int main(int argc, char *argv[]) {
     }
 
     // implement the UTF-8 analyzer here
-    int i = 0;
-    int validASCII = 1; 
-    while (*argv[i] != 0) {
-        if ((*argv[i] & 0b10000000) != 0) { validASCII = 0; }
-    }
-
-    printf("Valid ASCII: ");
-    if (validASCII == 1) { printf(" true\n"); } else { printf(" false\n");}
 }
