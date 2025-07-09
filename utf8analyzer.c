a74131f70846739aaae26655cd1bffe8edbe143d
Park <akp007@ieng6-203.ucsd.edu>
2025-07-09 12:07:15 -0700

    Valid Ascii Tester addition

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -11,4 +11,12 @@ int main(int argc, char *argv[]) {
     }
 
     // implement the UTF-8 analyzer here
+    int i = 0;
+    int validASCII = 1; 
+    while (*argv[i] != 0) {
+        if ((*argv[i] & 0b10000000) != 0) { validASCII = 0; }
+    }
+
+    printf("Valid ASCII: ");
+    if (validASCII == 1) { printf(" true\n"); } else { printf(" false\n");}
 }
